7e0e29233a266695c58d2cec4db24acfa4cedd38
Nico Garce <102405720+NicoGarce@users.noreply.github.com>
2024-09-05 19:17:43 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,11 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        public function boot(): void
-        {
             if ($this->app->environment('production')) {
                 URL::forceScheme('https');
             }
-        }
     }
 }
